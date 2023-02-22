87bf994d182e709285dd398cac92285685480d91
musty1406 <musty1406@gmail.com>
2023-02-22 16:47:44 +0300

    22/02/10 gün sonu

---
+++ javascript_examples/örnekler/user.js
@@ -0,0 +1,92 @@
+/*
+user nesnesi yaratalım json obje 
+
+user==>
+name
+surName
+username
+age
+password
+interest => 1den falz ilgi alanı olacak
+expenses ==> 1den fazla harcaması olacak
+address==> isiminde bir nesne içerecek 
+country
+city
+
+*/
+
+let user = {
+  name: "Mustafa",
+  surName: "Ozturk",
+  age: 35,
+  username: "musti",
+  password: "123",
+  interest: ["Müzik", "Seyahat", "Spor"],
+  expenses: [100, 60, 25, 850, 158],
+  address: {
+    country: "U.S.A",
+    city: "New York",
+  },
+};
+
+console.log(user.address.city);
+//user.username = "musty";
+console.log(user);
+
+// 1- harcamalr toplamını getiren fonksiyonu yazalım
+
+function getExpenses() {
+  let sum = 0;
+  user.expenses.forEach((x) => {
+    sum += x;
+  });
+  return sum;
+}
+const sumExpenses = () => {
+  let sum = user.expenses.reduce((x, y) => x + y);
+  return sum;
+};
+console.log(getExpenses());
+console.log(sumExpenses());
+
+const sumExpenses2 = (array) => {
+  let sum = array.reduce((x, y) => x + y);
+  return sum;
+};
+console.log(sumExpenses2(user.expenses));
+
+function getLastTwoExp() {
+  let index = user.expenses.length - 2;
+  return user.expenses.slice(index);
+}
+function getLastTwoExp2() {
+  return user.expenses.slice(-2);
+}
+
+function getLastTwoExp3() {
+  return user.expenses.reverse().slice(0, 2);
+}
+
+console.log(getLastTwoExp());
+console.log(getLastTwoExp2());
+console.log(getLastTwoExp3());
+console.log(user.expenses);
+// interest listesine 1.elamandan itibaren dışarıdan girdiğim 2 ilgi alnını eklemek istiyorum
+
+function addIntersts(interest1, interest2) {
+  user.interest.splice(1, 0, interest1, interest2);
+}
+
+addIntersts("Sinema", "Dans");
+
+console.log(user.interest);
+
+function login() {
+  let username = document.getElementById("myusername").value;
+  let pasword = document.getElementById("mypassword").value;
+  if (username === user.username && pasword === user.password) {
+    window.location.href = "/javascript_examples/%C3%B6rnekler/home.html";
+  } else {
+    alert("giriş basarısız");
+  }
+}
